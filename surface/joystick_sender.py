56679a0e6b7c1d6531ad45a8020c656134d1710c
Brennan <bmillerklugman@wit.edu>
2022-03-17 13:54:06 -0400

    Updated joystick

--- surface/joystick_sender.py
+++ surface/joystick_sender.py
@@ -29,125 +29,120 @@ import socket
 from wurov.msg import surface_command, io_request
 import time
 
-try:
-    publisher = rospy.Publisher('surface_command', surface_command, queue_size=3)
-    rospy.init_node('joystick_sender_'+socket.gethostname(), anonymous=False)  # Be effectively anonymous by naming the node after the hostname
-except rospy.exceptions.ROSInitException as e:
-    print("You've cancelled initialization of this node. Shutting down.")
-    sys.exit(0)
+class imu_data:
+    def __init__(self):
+        self.publisher = rospy.Publisher('surface_command', surface_command, queue_size=3)
+        rospy.init_node('joystick_sender_'+socket.gethostname(), anonymous=False)  # Be effectively anonymous by naming the node after the hostname
+
+        self.joystick = None
+
+        # We'll use this to try not to connect to the joystick too quickly.
+        rate = rospy.Rate(1)
+
+        os.environ["SDL_VIDEODRIVER"] = "dummy"
+        pygame.display.init()
+
+        try:
+            pygame.joystick.init()
+            while pygame.joystick.get_count() == 0:
+                rospy.logerr("No joystick connected!")
+                #pygame.joystick.quit()
+                pygame.joystick.init()
+                rate.sleep()
+            rospy.loginfo("Found a joystick to use.")
+            pygame.init()
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+        except pygame.error:
+            rospy.logerr("Failed to initialize joystick!")
+            sys.exit(1)
+
+        parser = argparse.ArgumentParser("Find a plugged in joystick and send it over /surface_command.")
+        parser.add_argument('--config_name', type=str, help='Set the name of the file we should use as a config (from '
+                                                            'within the config directory)')
+        self.args = parser.parse_args(rospy.myargv()[1:])
+
+        # roslaunch/rosrun executes this from the wrong directory, preventing us from calling the config import.
+        # By updating our python path via sys, we're able to tell it where to find this stuff.
+        # TODO: Make this conditional on a command line parameter.
+        rospkg = rospkg.RosPack()
+        sys.path.append(rospkg.get_path('wurov'))
+        import config
+
+        # Now that we're not using the rate to slow down our joystick connection, let's bring it to something we'll use.
+        rospy.Timer(rospy.Rate(10), self.update)
+        rospy.spin()
+
+    def update(self):
+            lastmsg = surface_command()
+
+            pygame.event.get()
+            horizontal_axis = self.joystick.get_axis(0)  # Horizontal: -1 is full left, 1 is full right
+            vertical_axis = self.joystick.get_axis(1)  # Vertical: -1 is full forward, 1 is full back
+            twist_axis = self.joystick.get_axis(2)  # Twist: -1 is full counter-clockwise, 1 is clockwise
+            lever_axis = self.joystick.get_axis(3)  # Lever: 1 is full down, -1 is full up
+            self.msg = surface_command()
+            self.msg.desired_trajectory.translation.x = -1 * horizontal_axis
+            self.msg.desired_trajectory.translation.y = vertical_axis  
+            self.msg.desired_trajectory.translation.z = -1 * lever_axis # Flipped: forward is negative, that's dumb
+            self.msg.desired_trajectory.orientation.yaw = -1 * twist_axis
+            
+            msg = self.config.simulate_peripherals.handle_peripherals(self.joystick, msg)
+            print(msg)
+            if self.different_msg(lastmsg, msg):
+                self.publisher.publish(msg)
+                lastmsg = msg
+
+    def hat_to_val(self, a, b):
+        if a == 0:
+            if b == 0:
+                return None
+            if b == 1:
+                return "top_front"
+            if b == -1:
+                return "top_left"
+        if a == 1:
+            if b == 0:
+                return "top_right"
+            if b == 1:
+                return "front_right"
+            if b == -1:
+                return "front_left"
+        if a == -1:
+            if b == 0:
+                return "top_back"
+            if b == 1:
+                return "back_left"
+            if b == -1:
+                return "back_right"
 
 
-def hat_to_val(a, b):
-    if a == 0:
-        if b == 0:
-            return None
-        if b == 1:
-            return "top_front"
-        if b == -1:
-            return "top_left"
-    if a == 1:
-        if b == 0:
-            return "top_right"
-        if b == 1:
-            return "front_right"
-        if b == -1:
-            return "front_left"
-    if a == -1:
-        if b == 0:
-            return "top_back"
-        if b == 1:
-            return "back_left"
-        if b == -1:
-            return "back_right"
+    def handle_peripherals(self, joystick_, msg_):
+        hat = joystick_.get_hat(0)
+        hat = self.hat_to_val(hat[0], hat[1])
+
+        io_request_ = io_request()
+        io_request_.executor = "individual_thruster_control"
+
+        if hat is not None:
+            io_request_.float = 0.75
+            io_request_.string = hat
+        else:
+            io_request_.float = 0.5
+            io_request_.string = "all"
+        self.msg.io_requests += (io_request_,)
+
+        return msg_  # If we wanted to do something with button presses, we could mess around with that sort of thing here.
 
 
-def handle_peripherals(joystick_, msg_):
-    hat = joystick_.get_hat(0)
-    hat = hat_to_val(hat[0], hat[1])
+    def different_msg(self, msg1, msg2):
+        if msg1 is None or msg2 is None:
+            return True
 
-    io_request_ = io_request()
-    io_request_.executor = "individual_thruster_control"
-
-    if hat is not None:
-        io_request_.float = 0.75
-        io_request_.string = hat
-    else:
-        io_request_.float = 0.5
-        io_request_.string = "all"
-    msg.io_requests += (io_request_,)
-
-    return msg_  # If we wanted to do something with button presses, we could mess around with that sort of thing here.
-
-
-def different_msg(msg1, msg2):
-    if msg1 is None or msg2 is None:
-        return True
-
-    return msg1.desired_trajectory.orientation != msg2.desired_trajectory.orientation or \
-           msg1.desired_trajectory.translation != msg2.desired_trajectory.translation or \
-           msg1.io_requests != msg2.io_requests
+        return msg1.desired_trajectory.orientation != msg2.desired_trajectory.orientation or \
+            msg1.desired_trajectory.translation != msg2.desired_trajectory.translation or \
+            msg1.io_requests != msg2.io_requests
 
 
 if __name__ == '__main__':
-    joystick = None
-
-    # We'll use this to try not to connect to the joystick too quickly.
-    rate = rospy.Rate(1)
-
-    os.environ["SDL_VIDEODRIVER"] = "dummy"
-    pygame.display.init()
-
-    try:
-        pygame.joystick.init()
-        while pygame.joystick.get_count() == 0:
-            rospy.logerr("No joystick connected!")
-            #pygame.joystick.quit()
-            pygame.joystick.init()
-            rate.sleep()
-        rospy.loginfo("Found a joystick to use.")
-        pygame.init()
-        joystick = pygame.joystick.Joystick(0)
-        joystick.init()
-    except pygame.error:
-        rospy.logerr("Failed to initialize joystick!")
-        sys.exit(1)
-
-    parser = argparse.ArgumentParser("Find a plugged in joystick and send it over /surface_command.")
-    parser.add_argument('--config_name', type=str, help='Set the name of the file we should use as a config (from '
-                                                        'within the config directory)')
-    args = parser.parse_args(rospy.myargv()[1:])
-
-    # roslaunch/rosrun executes this from the wrong directory, preventing us from calling the config import.
-    # By updating our python path via sys, we're able to tell it where to find this stuff.
-    # TODO: Make this conditional on a command line parameter.
-    rospkg = rospkg.RosPack()
-    sys.path.append(rospkg.get_path('wurov'))
-    import config
-
-    # Now that we're not using the rate to slow down our joystick connection, let's bring it to something we'll use.
-    rate = rospy.Rate(10)
-    lastmsg = surface_command()
-    while not rospy.is_shutdown():
-        try:
-            pygame.event.get()
-            horizontal_axis = joystick.get_axis(0)  # Horizontal: -1 is full left, 1 is full right
-            vertical_axis = joystick.get_axis(1)  # Vertical: -1 is full forward, 1 is full back
-            twist_axis = joystick.get_axis(2)  # Twist: -1 is full counter-clockwise, 1 is clockwise
-            lever_axis = joystick.get_axis(3)  # Lever: 1 is full down, -1 is full up
-            msg = surface_command()
-            msg.desired_trajectory.translation.x = -1 * horizontal_axis
-            msg.desired_trajectory.translation.y = vertical_axis  
-            msg.desired_trajectory.translation.z = -1 * lever_axis # Flipped: forward is negative, that's dumb
-            msg.desired_trajectory.orientation.yaw = -1 * twist_axis
-            
-            msg = config.simulate_peripherals.handle_peripherals(joystick, msg)
-            print(msg)
-            if different_msg(lastmsg, msg):
-                publisher.publish(msg)
-                lastmsg = msg
-
-            rate.sleep()
-
-        except KeyboardInterrupt:
-            rospy.loginfo("Got keyboard interrupt. Bye!")
-            sys.exit(0)
+    imu_data()
