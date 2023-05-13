9fed7a991ac776459f1f51d56f772641dcea73df
Tedi.q <tedi.qafko@gmail.com>
2023-05-12 23:14:17 -0400

    Update pca9685.py

--- plugins/motors/thruster/pca9685.py
+++ plugins/motors/thruster/pca9685.py
@@ -199,6 +199,27 @@ def light_callback(data):
     else:
         pca.channels[10].duty_cycle = MAX_PCA_INT_VAL
 
+test = False
+def grip_callback(data):
+    if data.data == True:
+        global test
+        test = not test
+        if test == True:
+            pca.channels[7].duty_cycle = MAX_PCA_INT_VAL
+        else:
+            pca.channels[7].duty_cycle = MIN_PCA_INT_VAL
+
+test1 = False
+def rotate_callback(data):
+    if data.data == True:
+        global test1
+        test1 = not test1
+        if test1 == True:
+            pca.channels[9].duty_cycle = MAX_PCA_INT_VAL
+        else:
+            pca.channels[9].duty_cycle = MIN_PCA_INT_VAL
+
+
 def sensor_callback(data):
     """
     If there are sensors on your thrusters, here's where you can deal with that.
@@ -206,27 +227,6 @@ def sensor_callback(data):
     if data.estop:
         rospy.logerr("estop triggered on " + data.thruster)
         kill_thruster(data.thruster)
-
-test = False
-def grip_callback(data):
-    if data.data == True:
-        global test 
-        test = not test
-        if test == True:
-            pca.channels[7].duty_cycle = (MAX_PCA_INT_VAL-MIN_PCA_INT_VAL)/2
-        else:
-            pca.channels[7].duty_cycle = MAX_PCA_INT_VAL
-
-test1 = False
-def rotate_collback(data):
-    if data.data == True:
-        global test1 
-        test1 = not test1
-        if test1 == True:
-            pca.channels[9].duty_cycle = (MAX_PCA_INT_VAL-MIN_PCA_INT_VAL)/2
-        else:
-            pca.channels[9].duty_cycle = MAX_PCA_INT_VAL
-
 
 def persistent_pca(channel, pwm):
     """
@@ -352,7 +352,7 @@ def listener(arguments):
     rospy.Subscriber('arbitrary_pca_commands', arbitrary_pca_commands, arbitrary_pca_callback)
     rospy.Subscriber('light_control', Bool, light_callback)
     rospy.Subscriber('grip_close', Bool, grip_callback)
-    rospy.Subscriber('grip_rotate', Bool, rotate_collback)
+    rospy.Subscriber('grip_rotate', Bool, rotate_callback)
 
     if pca is None:
         # Defaulting to a simulation mode (like we do here) is a good default to allow the usage of only one system.
